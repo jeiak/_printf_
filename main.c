857a4b4a5e90c82c3e7db191b4333663446d6335
Jessica Sanchez Padilla <jesssanc@c3r5s6.42urduliz.com>
2024-12-12 13:17:26 +0100

    main

--- main.c
+++ main.c
@@ -6,57 +6,65 @@
 /*   By: jesssanc <jesssanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 14:04:01 by jesssanc          #+#    #+#             */
-/*   Updated: 2024/12/12 10:18:26 by jesssanc         ###   ########.fr       */
+/*   Updated: 2024/12/12 11:56:14 by jesssanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h> // Para usar printf y compararlo con ft_printf
+#include <stdio.h>
 
-int	main(void)
+int main()
 {
-	int	ret1, ret2;
+    int ret1, ret2;
 
-	// Prueba con caracteres
-	ret1 = ft_printf("Mi ft_printf: Caracter: %c\n", 'A');
-	ret2 = printf("Origi printf: Caracter: %c\n", 'A');
-	printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
+    // Casos normales
+    int a = 100;
+    char *str = "Hello, World!";
+    ret1 = ft_printf("Mi ft_printf: Caracter: %c, String: %s, Entero: %d, Hexadecimal: %x\n", 'A', str, a, a);
+    ret2 = printf("Origi printf: Caracter: %c, String: %s, Entero: %d, Hexadecimal: %x\n", 'A', str, a, a);
+    printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
 
-	// Prueba con cadenas
-	ret1 = ft_printf("Mi ft_printf: Cadena: %s\n", "Hola mundo");
-	ret2 = printf("Origi printf: Cadena: %s\n", "Hola mundo");
-	printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
+    // Casos con punteros
+    ret1 = ft_printf("Mi ft_printf: Puntero: %p\n", &a);
+    ret2 = printf("Origi printf: Puntero: %p\n", &a);
+    printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
 
-	// Prueba con enteros
-	ret1 = ft_printf("Mi ft_printf: Entero: %d\n", 42);
-	ret2 = printf("Origi printf: Entero: %d\n", 42);
-	printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
+    // Puntero nulo
+    ret1 = ft_printf("Mi ft_printf: Puntero nulo: %p\n", NULL);
+    ret2 = printf("Origi printf: Puntero nulo: %p\n", NULL);
+    printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
 
-	// Prueba con enteros sin signo
-	ret1 = ft_printf("Mi ft_printf: Entero sin signo: %u\n", 4294967295U);
-	ret2 = printf("Origi printf: Entero sin signo: %u\n", 4294967295U);
-	printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
+    // Casos con valores negativos y cero
+    ret1 = ft_printf("Mi ft_printf: Entero negativo: %d, Cero: %d\n", -42, 0);
+    ret2 = printf("Origi printf: Entero negativo: %d, Cero: %d\n", -42, 0);
+    printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
 
-	// Prueba con hexadecimales
-	ret1 = ft_printf("Mi ft_printf: Hexadecimal minúsculas: %x\n", 250);
-	ret2 = printf("Origi printf: Hexadecimal minúsculas: %x\n", 250);
-	printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
+    // Casos de errores en cadenas de formato
+    ret1 = ft_printf("Mi ft_printf: Formato incorrecto: %z\n");  // %z no es un especificador válido
+    ret2 = printf("Origi printf: Formato incorrecto: %z\n");
+    printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
 
-	ret1 = ft_printf("Mi ft_printf: Hexadecimal mayúsculas: %X\n", 255);
-	ret2 = printf("Origi printf: Hexadecimal mayúsculas: %X\n", 255);
-	printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
+    // Prueba con valores extremos
+    unsigned int big_number = 4294967295;  // 4,294,967,295
+    ret1 = ft_printf("Mi ft_printf: Número grande: %u\n", big_number);
+    ret2 = printf("Origi printf: Número grande: %u\n", big_number);
+    printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
 
-	// Prueba con punteros
-	int	a = 100;
-	ret1 = ft_printf("Mi ft_printf: Puntero: %p\n", NULL);
-	ret2 = printf("Origi printf: Puntero: %p\n", NULL);
-	printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
+    // Casos con % no seguido de especificador
+    ret1 = ft_printf("Mi ft_printf: Porcentaje: %%\n");
+    ret2 = printf("Origi printf: Porcentaje: %%\n");
+    printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
 
-	// Prueba con porcentaje
-	ret1 = ft_printf("Mi ft_printf: Porcentaje: %%\n");
-	ret2 = printf("Origi printf: Porcentaje: %%\n");
-	printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
+    // Cadena vacía
+    ret1 = ft_printf("Mi ft_printf: Cadena vacía: %s\n", "");
+    ret2 = printf("Origi printf: Cadena vacía: %s\n", "");
+    printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
 
-	return (0);
+    // Caracter nulo
+    ret1 = ft_printf("Mi ft_printf: Caracter nulo: %c\n", '\0');
+    ret2 = printf("Origi printf: Caracter nulo: %c\n", '\0');
+    printf("ft_printf devolvió: %d, printf devolvió: %d\n", ret1, ret2);
+
+    return 0;
 }
 // gcc -o ft_printf_test main.c -L. -lftprintf  //
